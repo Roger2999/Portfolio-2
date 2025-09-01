f780b18651f64776f72cdc9c96caae0f215f4533
roger <rogergutierrezmartinez900@gmail.com>
2025-09-01 15:35:21 -0400

    install tailwindcss v3 & daisiUI v5 for compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import "./index.css"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PortfolioRouter } from "./PortfolioRouter";
 const queryClient = new QueryClient();
