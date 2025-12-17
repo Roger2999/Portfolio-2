1b3a788effd42e1f89ea9d56084ef995786deada
roger2999 <rogergutierrezmartinez900@gmail.com>
2025-12-16 22:45:17 -0500

    Some changes are being made to the skills page.

--- src/data/skillsData.ts
+++ src/data/skillsData.ts
@@ -1,16 +1,56 @@
-
-
 export const frontendSkills = [
-  { id:"1", name: "HTML",description:"HTML DESCRIPCION",icon:"https://www.svgrepo.com/show/349402/html5.svg", level: 5 },
-  { id:"2", name: "CSS",description:"CSS DESCRIPCION",icon:"https://www.svgrepo.com/show/452185/css-3.svg", level: 3 },
-  { id:"3", name: "React",description:[
+  {
+    id: "1",
+    name: "HTML",
+    description: "HTML DESCRIPCION",
+    icon: "https://www.svgrepo.com/show/349402/html5.svg",
+    level: 5,
+  },
+  {
+    id: "2",
+    name: "CSS",
+    description: [
+      "CSS Modules",
+      "Tailwindcss",
+      "Bootstrap",
+      "DaisyUi",
+      "Material UI",
+    ],
+    icon: "https://www.svgrepo.com/show/452185/css-3.svg",
+    level: 3,
+  },
+  {
+    id: "3",
+    name: "React",
+    description: [
       "Zustand",
-      "Context Api",
+      "Context API",
       "React/Tanstack Query",
       "React Hook Form + Zod",
-      "React Router DOM v7",
-      "Git",
-  ],icon:"https://www.svgrepo.com/show/493719/react-javascript-js-framework-facebook.svg", level: 4 },
-  { id:"4", name: "TypeScript",description:"TS DESCRIPCION",icon:"https://www.svgrepo.com/show/354478/typescript-icon.svg", level: 4 },
-  
+      "React Router DOM",
+    ],
+    icon: "https://www.svgrepo.com/show/493719/react-javascript-js-framework-facebook.svg",
+    level: 4,
+  },
+  {
+    id: "4",
+    name: "TypeScript",
+    description: "TS DESCRIPCION",
+    icon: "https://www.svgrepo.com/show/354478/typescript-icon.svg",
+    level: 4,
+  },
+  {
+    id: "4",
+    name: "GIT",
+    description: "TS DESCRIPCION",
+    icon: "https://www.svgrepo.com/show/353782/git-icon.svg",
+    level: 4,
+  },
+  {
+    id: "4",
+    name: "Supabase",
+    description: "TS DESCRIPCION",
+    icon: "https://www.vectorlogo.zone/logos/supabase/supabase-icon.svg",
+    level: 3,
+  },
 ];
