e4d3345a2fb2323f39586000697337a57818a234
roger2999 <rogergutierrezmartinez900@gmail.com>
2025-12-18 08:59:30 -0500

    Added arrow icon to contact page.

--- src/public/pages/ContactPage/ContactPage.tsx
+++ src/public/pages/ContactPage/ContactPage.tsx
@@ -10,31 +10,41 @@ export const ContactPage = () => {
         className={`contact-card-container flex flex-col w-[500px] max-w-[80%]`}
       >
         <div
-          className={`contact-card flex flex-col glass rounded-2xl font-mono p-6 w-full ${
+          className={`contact-card flex flex-col glass rounded-2xl font-mono w-full ${
             theme === "light" ? "bg-gray-300" : "text-gray-50"
           }`}
         >
-          <h2 className="text-2xl font-bold mb-4 text-center">
-            Información de contacto
-          </h2>
-          <ul className="w-full space-y-2 text-start p-4">
-            <li>
-              <span>Nombre: </span> Roger Gutiérrez Martínez
-            </li>
-            <li>
-              <span>País: </span> Cuba
-            </li>
-            <li>
-              <span>Ciudad: </span> Matanzas
-            </li>
-            <li className="break-words">
-              <span>Correo: </span>
-              rogergutierrezmartinez900@gmail.com
-            </li>
-            <li>
-              <span>Teléfono:</span> +53 54849352
-            </li>
-          </ul>
+          <div className="contact-info p-6">
+            <h2 className="text-2xl font-bold mb-4 text-center">
+              Información de contacto
+            </h2>
+            <ul className="w-full space-y-2 text-start p-4">
+              <li>
+                <span>Nombre: </span> Roger Gutiérrez Martínez
+              </li>
+              <li>
+                <span>País: </span> Cuba
+              </li>
+              <li>
+                <span>Ciudad: </span> Matanzas
+              </li>
+              <li className="break-words">
+                <span>Correo: </span>
+                rogergutierrezmartinez900@gmail.com
+              </li>
+              <li>
+                <span>Teléfono:</span> +53 54849352
+              </li>
+            </ul>
+          </div>
+          <div className="arrow flex justify-center items-center  bg-white/20 text-center font-bold backdrop-blur-2xl border border-white/40 transition-all duration-200 ease hover:bg-white/5">
+            <img
+              src="src/assets/arrow-icon.svg"
+              alt="arrow-icon"
+              width={40}
+              height={40}
+            />
+          </div>
         </div>
 
         <div className="card-details flex flex-wrap gap-4 sm:gap-10 p-4 items-center justify-center">
